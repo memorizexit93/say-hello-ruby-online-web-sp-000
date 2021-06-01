fd68c42aa4b2cd3adb40e9b4f5d6360a0f66a0d7
Kathryn Senack <katbrooke0219@gmail.com>
2021-06-01 19:08:17 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 def say_hello
-  puts say_hello("Hello, #(name)!")
-  puts say_hello(Ruby Programmer)
-  puts say_hello(Kathryn)
+  puts "Hello, #(name)!")
+  puts (Ruby Programmer)
+  puts (Kathryn)
 end
